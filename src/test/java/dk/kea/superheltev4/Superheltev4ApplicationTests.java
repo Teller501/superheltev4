abf20bf8fb3041b6cee87c886cc7464f2336a986
Teller501 <ande777j@stud.kea.dk>
2023-03-07 09:50:17 +0100

    initial commit

---
+++ src/test/java/dk/kea/superheltev4/Superheltev4ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.superheltev4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Superheltev4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
